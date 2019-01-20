8d195013dbf5398a41b4171a176422aafbe85228
3589Xl986 <46425597+3589Xl986@users.noreply.github.com>
2019-01-20 13:59:37 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+//
+//var nav = doucment.querySelector('.navbar-brand img');
+//
+//window.addEventListener('scroll', function(){
+//    if(window.pageYOffset > 100){
+//        nav.classList += ' sml-logo'
+//    } else {
+//        nav.classList = 'navbar-brand img';
+//    }
+//})
+
+//$(window).on('scroll', function(){
+//    if ($(window).scrollTop()){
+//        $('.navbar-brand img').addClass('sml-logo');
+//    }
+//    else {
+//        $('.navbar-brand img').removeClass('sml-logo');
+//    }
+//})
+.def-cols{
+    color: #90C74D;
+    color: #90D12A;
+    color: #298429;
+    color: #EFF9E0;
+    /*=============*/
+    color: #96D701;
+    color: #024436;
+    color: #005D62;
+    color: #8EB8EA;
+    color: #F5E85B;
+}
